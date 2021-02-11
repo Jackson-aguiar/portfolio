a1dbd85c33d1b68b895dc5c82bbaa454511f946c
Jackson-aguiar <jackson.s.aguiar@gmail.com>
2021-02-11 14:02:40 -0300

    Alteração nas migrations

--- database/migrations/2021_01_29_175934_create_status_table.php
+++ database/migrations/2021_01_29_175934_create_status_table.php
@@ -15,7 +15,7 @@ class CreateStatusTable extends Migration
     {
         Schema::create('status', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name', 50);
             $table->timestamps();
         });
     }
